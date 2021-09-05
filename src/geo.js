a742a55365ea81494dd6add8975417e1078f704a
Androz2091 <androz2091@gmail.com>
2021-09-05 18:32:53 +0200

    :sparkles: Use proxy for geo function

--- src/geo.js
+++ src/geo.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 function geo(lat, long) {
     return new Promise((resolve, reject) => {
@@ -15,7 +16,9 @@ function geo(lat, long) {
                 // eslint-disable-next-line max-len
                 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
                 'Content-Length': data.length
-            }
+            },
+            // eslint-disable-next-line no-process-env
+            agent: process.env.PRONOTE_API_PROXY ? new HttpsProxyAgent(process.env.PRONOTE_API_PROXY) : undefined
         }
 
         let incomingData = '';
